53eecde11b89b837795aa4abecc3955f74b3852f
Eric Loos <ericloos00@gmail.com>
2017-06-13 13:00:00 -0500

    added frontend assets and home index

---
+++ salesApp/app/assets/javascripts/app.js
@@ -0,0 +1,8 @@
+// angular.module('App').config(function($stateProvider, $urlRouterProvider, $locationProvider){
+
+//   // set the * route
+//   // if nothing matches user will be redirected to `/`
+//   $urlRouterProvider.otherwise('/');
+//   $locationProvider.html5Mode(false);
+
+// // })
