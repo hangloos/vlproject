67e9a4a6aba1e3da0ca742063a55bdd1bcd94304
Eric Loos <ericloos00@gmail.com>
2017-06-13 13:09:05 -0500

    bootstrap is working properly

--- salesApp/app/assets/javascripts/application.js
+++ salesApp/app/assets/javascripts/application.js
@@ -11,5 +11,10 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
+//= require angular
+//= require angular-ui-router
+//= require angular-devise
+//= require angular-rails-templates
+//= require bootstrap
+//= require moment
 //= require_tree .
