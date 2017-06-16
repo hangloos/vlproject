6d896c4da9d8be31ed4d20afdf58cd92f111ed49
Raman Jaswal <raman_jaswal@esferasoft.com>
2017-06-16 11:48:07 +0530

    Fixed angular js dependency error

--- salesApp/app/assets/javascripts/app.js
+++ salesApp/app/assets/javascripts/app.js
@@ -4,9 +4,9 @@
  angular
       .module('app', ['templates', 'Devise', 'ui.router', 'angularModalService'])
       .config(function($httpProvider)  {
-        // for CSRF errors
-       $httpProvider.defaults.headers.common['X-CSRF-TOKEN'] = 
-       $('meta[name=csrf-token]').attr('content')
+           // for CSRF errors
+	       $httpProvider.defaults.headers.common['X-CSRF-TOKEN'] = 
+	       $('meta[name=csrf-token]').attr('content')
        })
 
      }())
