6d09d3870e6a89ce779255d954ae456137bc9dd4
Eric Loos <ericloos00@gmail.com>
2017-06-29 08:23:57 -0500

    testing prod emails

--- salesApp/app/mailers/interview_setup.rb
+++ salesApp/app/mailers/interview_setup.rb
@@ -3,8 +3,9 @@ class InterviewSetup < ApplicationMailer
   def interview_email(user,company)
       @user = user
       @company = company
+      ccrecipients = ['ericloos00@gmail.com', 'ericloos00@gmail.com']
       @url  = 'http://www.gmail.com'
-      mail(to: 'ericloos00@gmail.com', cc: ['ericloos00@gmail.com','ericloos00@gmail.com'], subject: 'A Victory Lap Partner Wants To Start Talking!')
+      mail(to: 'ericloos00@gmail.com', cc: ccrecipients, subject: 'A Victory Lap Partner Wants To Start Talking!')
       # to: @user.email , bcc: 'brian and kevin', cc: @company.email, from: info@victorylap.io
    end
 
@@ -12,7 +13,7 @@ class InterviewSetup < ApplicationMailer
    def signup_email(user)
       @user = user
       @url  = 'http://www.gmail.com'
-      mail(to: 'ericloos00@gmail.com', cc: 'brian@victorylap.io', subject: 'Sign Up Confirmation - Victory Lap Portal')
+      mail(to: @user.email, cc: 'brian@victorylap.io', subject: 'Sign Up Confirmation - Victory Lap Portal')
       # to: @user.email , bcc: 'brian and kevin', cc: @company.email, from: info@victorylap.io
    end
 
